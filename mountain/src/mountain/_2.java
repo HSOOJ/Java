f7a055635f866ee669cc197383996df83ef18e20
HSOOJ <blue8959@naver.com>
2016-09-06 20:03:26 +0900

    .

--- mountain/src/mountain/_2.java
+++ mountain/src/mountain/_2.java
@@ -2,6 +2,12 @@ package mountain;
 
 public class _2 {
 	public static void main(String[] args) {
-
+		int count = 1;
+		for(int i = 0; i < 3; i++){
+			for(int j = 3 ; j > i ; j--){
+				System.out.print(count);
+				count++;
+			}System.out.print("\n");
+		}
 	}
 }
---
+++ mountain/src/mountain/_3.java
@@ -0,0 +1,19 @@
+package mountain;
+
+public class _3 {
+
+	public static void main(String[] args) {
+		int count = 1;
+		
+		for(int i = 0; i < 3; i++){
+			for (int j = 2 - i ; j > 0 ; j -- )
+				System.out.print(" ");
+			
+			for(int j = 0; j < i + 1; j++){
+				System.out.print(count);
+				count++;
+			}System.out.print("\n");
+
+		}
+	}
+}
